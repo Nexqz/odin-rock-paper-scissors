5e08a68cba77d1cb23d373b1dac95fe6e2a2acdc
qiu <149208052+Nexqz@users.noreply.github.com>
2026-06-28 21:19:19 -0400

    feat: add game logic of winning and losing

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let value = Math.floor(Math.random() * 3);
   switch (value) {
     case 0:
-      return "Rock";
+      return "rock";
       break;
     case 1:
-      return "Paper";
+      return "paper";
       break;
     case 2:
-      return "Scissors";
+      return "scissors";
       break;
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let value = prompt("Choose Rock, Paper, or Scissors");
+  return value.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(
+    `Human Choice: ${humanChoice}, Computer Choice: ${computerChoice}`,
+  );
+  if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      return "you win";
+    } else if (computerChoice === "paper") {
+      return "you lose";
+    } else {
+      return "tie";
+    }
+  }
+
+  if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "you win";
+    } else if (computerChoice === "scissors") {
+      return "you lose";
+    } else {
+      return "tie";
+    }
+  }
+
+  if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      return "you win";
+    } else if (computerChoice === "rock") {
+      return "you lose";
+    } else {
+      return "tie";
+    }
+  }
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
